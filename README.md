1674d37f10c732c835701272cac928c2a312d920
Italo Silveira <italobrian@gmail.com>
2024-06-13 17:29:17 -0300

    Architecture CQRS FullLIB (.NET 8.0)

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # template-architecture-cqrsflib
-Architecture CQRS FullLIB .NET 8.0
+Architecture CQRS FullLIB (.NET 8.0)
+
+#### Migrations
+
+Go to "BAYSOFT.Infrastructures.Data" project folder and open cmd
+
+> dotnet ef --startup-project ../BAYSOFT.Presentations.WebAPI migrations add InitialMigrationDefaultDbContext -c DefaultDbContext -o Default/Migrations
--- src/BAYSOFT.Middleware/Configurations.cs
+++ src/BAYSOFT.Middleware/Configurations.cs
@@ -1,4 +1,5 @@
-﻿using BAYSOFT.Middleware.AddServices;
+﻿using BAYSOFT.Abstractions.Crosscutting.InheritStringLocalization;
+using BAYSOFT.Middleware.AddServices;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
@@ -52,6 +53,52 @@ namespace BAYSOFT.Middleware
             //app.UseAuthorization();
             // YOUR CODE GOES HERE
             return app;
-        }
-    }
+		}
+		#region TESTS
+		public static IServiceCollection AddMiddlewareTest(this IServiceCollection services, IConfiguration configuration, Assembly presentationAssembly)
+		{
+			services.AddLocalization();
+
+			services.AddDbContextsTest(configuration);
+			services.AddSpecifications();
+			services.AddEntityValidations();
+			services.AddDomainValidations();
+			services.AddDomainServices();
+
+			var assemblyApplication = AppDomain.CurrentDomain.Load("BAYSOFT.Core.Application");
+			var assemblyDomain = AppDomain.CurrentDomain.Load("BAYSOFT.Core.Domain");
+			var assemblyInfrastructuresServices = AppDomain.CurrentDomain.Load("BAYSOFT.Infrastructures.Services");
+			services.AddMediatR(options => options.RegisterServicesFromAssemblies(assemblyApplication, assemblyDomain, assemblyInfrastructuresServices));
+
+			services.AddModelWrapper()
+				.AddDefaultReturnedCollectionSize(10)
+				.AddMinimumReturnedCollectionSize(1)
+				.AddMaximumReturnedCollectionSize(100)
+				.AddQueryTermsMinimumSize(3)
+				.AddSuppressedTerms(new string[] { "the" });
+
+			services.AddInheritStringLocalizerFactory();
+
+			// YOUR CODE GOES HERE
+			return services;
+		}
+
+		public static IApplicationBuilder UseMiddlewareTest(this IApplicationBuilder app)
+		{
+			var supportedCultures = new string[] { "en-US", "pt-BR" };
+
+			var localizationOptions = new RequestLocalizationOptions()
+				.SetDefaultCulture(supportedCultures[0])
+				.AddSupportedCultures(supportedCultures)
+				.AddSupportedUICultures(supportedCultures);
+
+			app.UseRequestLocalization(localizationOptions);
+
+			//app.UseAuthentication();
+			//app.UseAuthorization();
+			// YOUR CODE GOES HERE
+			return app;
+		}
+		#endregion
+	}
 }
